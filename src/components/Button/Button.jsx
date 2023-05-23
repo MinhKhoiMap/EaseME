f08587b823b3829c2db7e2c89b9b61f76e1d7e93
MinhKhoiMap <helloiamkhoi@gmail.com>
2023-05-24 01:24:39 +0700

    complete survey test (no score at the end of test)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,12 @@
 import "./Button.css";
 
-const Button = ({ text, borderWidth, handelOnClick }) => {
+const Button = ({ text, borderWidth, handelOnClick, props }) => {
   return (
     <button
       className="btn"
       onClick={handelOnClick}
       style={{ borderWidth: borderWidth }}
+      {...props}
     >
       {text}
     </button>
--- src/pages/Survey/Survey.jsx
+++ src/pages/Survey/Survey.jsx
@@ -1,8 +1,53 @@
+import "./Survey.css";
+import QuestionGroup from "./QuestionGroup/QuestionGroup";
+
+import { useState } from "react";
 
 const Survey = () => {
-  return (
-    <div>Survey</div>
-  )
-}
+  const label = [
+    {
+      text: "Cau co on khong?",
+      answersList: ["Kó", "Không", "Tớ không biết nữa"],
+    },
+    {
+      text: "Cau co can giup do ngay khong?",
+      answersList: ["Goi den hotline ho tro", "Khong can dau"],
+    },
+    {
+      text: "Dao nay cau hay thay lo lang chu?",
+      answersList: ["Gan nhu moi ngay", "Doi khi", "Thuong xuyen", "Khong co"],
+    },
+  ];
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
-export default Survey
+  const test = (id) => {
+    console.log(id, "id");
+    if (id < label.length) {
+      return (
+        <QuestionGroup
+          label={label[id].text}
+          answersList={label[id].answersList}
+          handleChooseAnswer={(e) => {
+            setScore((prev) => prev + Number(e.target.attributes.data.value));
+            setSurvey(() => test(id + 1));
+            // console.log([e.target], [e.target.attributes.data.value]);
+          }}
+          key={id}
+        />
+      );
+    } else {
+      setShowScore(true);
+    }
+  };
+  const [survey, setSurvey] = useState(test(0));
+
+  return (
+    <div className="survey-test__wrapper">
+      {survey}
+      {showScore && <div style={{ fontSize: "2.2rem" }}>Score: {score}</div>}
+    </div>
+  );
+};
+
+export default Survey;
